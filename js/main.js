f3b6c2cc19c52607aa9c0cfbdf3e7b8a8bf0c388
mercury <90138904+mxrcury@users.noreply.github.com>
2022-04-17 19:37:40 +0300

    site-2

---
+++ js/main.js
@@ -0,0 +1,23 @@
+(function(){
+    const header = document.querySelector('.header');
+    window.onscroll = () =>{
+        if(window.pageYOffset > 50){
+            header.classList.add('header_active')
+        }else{
+            header.classList.remove('header_active')
+        }
+    }
+}());
+
+(function(){
+const headerNav = document.querySelector('.header_nav');
+const burgerItem = document.querySelector('.burger');
+const closeItem = document.querySelector('.header_nav-close')
+
+burgerItem.addEventListener('click', () =>{
+    headerNav.classList.add('header_nav_active')
+});
+closeItem.addEventListener('click', () =>{
+    headerNav.classList.remove('header_nav_active')
+});
+}());
